bc83fe3abc78785e524207e9c77d229b8de2f4c1
phillcp <filipecabralpinto@gmail.com>
2025-12-15 14:05:31 +0000

    command parse fix

--- dep/builtins/echo.d
+++
@@ -1,8 +0,0 @@
-obj/builtins/echo.o: src/builtins/echo.c inc/minishell.h \
- lib/libft/inc/libft.h inc/parser.h
-
-inc/minishell.h:
-
-lib/libft/inc/libft.h:
-
-inc/parser.h:
--- dep/builtins/exit.d
+++
@@ -1,8 +0,0 @@
-obj/builtins/exit.o: src/builtins/exit.c inc/minishell.h \
- lib/libft/inc/libft.h inc/parser.h
-
-inc/minishell.h:
-
-lib/libft/inc/libft.h:
-
-inc/parser.h:
